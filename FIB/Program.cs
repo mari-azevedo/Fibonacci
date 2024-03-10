f48594570a572dd0a08cb19fc05ae00c766c9e78
MARIA_EDUARDA AZEVEDO <barbosa.eduarda@outlook.com.br>
2024-03-10 19:12:12 -0300

    Adicionar arquivos de projeto.

---
+++ FIB/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static bool Verificando(int numero)
+    {
+        int a = 0, b = 1;
+
+        while (a <= numero)
+        {
+            if (a == numero)
+                return true;
+
+            int temp = a;
+            a = b;
+            b = temp + b;
+        }
+
+        return false;
+    }
+
+    static void Main()
+    {
+        Console.Write("Informe um número: ");
+        int numeroInformado = Convert.ToInt32(Console.ReadLine());
+
+        if (Verificando(numeroInformado))
+            Console.WriteLine($"O número {numeroInformado} pertence à sequência de Fibonacci.");
+        else
+            Console.WriteLine($"O número {numeroInformado} não pertence à sequência de Fibonacci.");
+    }
+}
